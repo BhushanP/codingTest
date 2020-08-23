2037accee5d7a18442819560c32080d44cdbf6af
BhushanP <bhushan.pote@gmail.com>
2020-08-23 12:20:01 +0530

    Added Exception handling

--- promotion-engine/src/main/java/com/xyz/ecommerce/promotionengine/controllers/CartsController.java
+++ promotion-engine/src/main/java/com/xyz/ecommerce/promotionengine/controllers/CartsController.java
@@ -22,37 +22,43 @@ public class CartsController {
 	CartServiceImpl cartServiceImpl;
 
 	@RequestMapping(method = RequestMethod.POST, value = "/{cartId}")
-	public @ResponseBody ResponseEntity<Cart> addItemToCart(@PathVariable Integer cartId,@RequestBody CartItem item) {
-		Cart createdItem = cartServiceImpl.addItemToCart(cartId,item);
+	public @ResponseBody ResponseEntity<Cart> addItemToCart(@PathVariable Integer cartId, @RequestBody CartItem item) {
+		Cart createdItem = cartServiceImpl.addItemToCart(cartId, item);
 		return ResponseEntity.status(HttpStatus.OK).body(createdItem);
 	}
-	
 
 	@RequestMapping(method = RequestMethod.GET, value = "/{cartId}")
-	public @ResponseBody ResponseEntity<Cart>  viewCart(@PathVariable Integer cartId) {
-		Cart cart=null;
+	public @ResponseBody ResponseEntity viewCart(@PathVariable Integer cartId) {
+		Cart cart = null;
 		try {
 			cart = cartServiceImpl.viewCart(cartId);
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cart Not Found");
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(cart);
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "/{cartId}/{itemId}")
-	public @ResponseBody ResponseEntity<Cart> deleteCart(@PathVariable Integer cartId,@PathVariable String itemId) {
-		Cart updateCart=null;
+	public @ResponseBody ResponseEntity deleteCart(@PathVariable Integer cartId, @PathVariable String itemId) {
+		Cart updateCart = null;
 		try {
 			updateCart = cartServiceImpl.deleteItemFromCart(cartId, itemId);
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cart is Empty");
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(updateCart);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/{cartId}/{promoCode}")
-	public @ResponseBody ResponseEntity<Cart> applyPromocodeOnCart(@PathVariable Integer cartId,@PathVariable String promoCode) throws Exception {
-		Cart createdItem = cartServiceImpl.applyPromoCodeOnCart(cartId, promoCode);
+	public @ResponseBody ResponseEntity applyPromocodeOnCart(@PathVariable Integer cartId,
+			@PathVariable String promoCode) throws Exception {
+		Cart createdItem = null;
+		try {
+			createdItem = cartServiceImpl.applyPromoCodeOnCart(cartId, promoCode);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
 		return ResponseEntity.status(HttpStatus.OK).body(createdItem);
+
 	}
 }
